638370d1bab5b3dcff6104fdd547285f8c3f304c
kamy <130361402+KamyEsm@users.noreply.github.com>
2026-02-20 02:01:27 +0330

    update role section

--- src/main/java/org/example/onlineshop/DTO/RoleDTO.java
+++ src/main/java/org/example/onlineshop/DTO/RoleDTO.java
@@ -1,6 +1,8 @@
 package org.example.onlineshop.DTO;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,7 +14,11 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RoleDTO {
+    @NotBlank
+    @Size(min = 2, max = 40)
     private String name;
+    @Size(min = 2, max = 40)
+    @NotBlank
     private String description;
     private List<String> usernames;
 }
